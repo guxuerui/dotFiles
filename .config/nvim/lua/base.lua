64744dc522394accc418243694d51e9c555824a6
guxuerui <im.guxuerui@gmail.com>
2022-11-01 21:53:46 +0800

    update nvim base.lua config

--- .config/nvim/lua/base.lua
+++ .config/nvim/lua/base.lua
@@ -34,11 +34,5 @@ vim.cmd([[let &t_Cs = "\e[4:3m"]])
 vim.cmd([[let &t_Ce = "\e[4:0m"]])
 -- but this dosen't work on iTerm2. Need to verify.
 
--- Turn off paste mode when leaving insert
-vim.api.nvim_create_autocmd("InsertLeave", {
-  pattern = '*',
-  command = "set nopaste"
-})
-
 -- Add asterisks in block comments
 vim.opt.formatoptions:append { 'r' }
