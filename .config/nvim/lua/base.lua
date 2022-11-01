e6a6df1c4639313f2273ad0eb6f300368155acc6
guxuerui <im.guxuerui@gmail.com>
2022-11-01 23:49:59 +0800

    update files: base.lua

--- .config/nvim/lua/base.lua
+++ .config/nvim/lua/base.lua
@@ -1,11 +1,20 @@
 vim.cmd('autocmd!')
+vim.cmd('syntax on')
 
 vim.scriptencoding = 'utf-8'
 vim.opt.encoding = 'utf-8'
 vim.opt.fileencoding = 'utf-8'
 
+if (vim.fn.has('termguicolors') = 1) then
+  vim.opt.termguicolors = true
+end
 vim.wo.number = true
-
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.shiftround = true
+vim.opt.updatetime = 100
+vim.opt.cursorline = true
+vim.opt.autowrite = true
 vim.opt.title = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
@@ -24,8 +33,9 @@ vim.opt.smarttab = true
 vim.opt.breakindent = true
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
 vim.opt.wrap = false -- No wrap lines
-vim.opt.backspace = 'start, eol, indent'
+vim.opt.backspace = 'start,eol,indent'
 vim.opt.path:append { '**' } -- Finding files - Search down into subfolders
 vim.opt.wildignore:append { '*/node_modules/*' }
 
@@ -36,3 +46,20 @@ vim.cmd([[let &t_Ce = "\e[4:0m"]])
 
 -- Add asterisks in block comments
 vim.opt.formatoptions:append { 'r' }
+
+-- disable some useles standard plugins to save startup time
+-- these features have been better covered by plugins
+vim.g.loaded_matchparen        = 1
+vim.g.loaded_matchit           = 1
+vim.g.loaded_logiPat           = 1
+vim.g.loaded_rrhelper          = 1
+vim.g.loaded_tarPlugin         = 1
+vim.g.loaded_gzip              = 1
+vim.g.loaded_zipPlugin         = 1
+vim.g.loaded_2html_plugin      = 1
+vim.g.loaded_shada_plugin      = 1
+vim.g.loaded_spellfile_plugin  = 1
+vim.g.loaded_netrw             = 1
+vim.g.loaded_netrwPlugin       = 1
+vim.g.loaded_tutor_mode_plugin = 1
+vim.g.loaded_remote_plugins    = 1
