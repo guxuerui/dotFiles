54531d9b5825059a27e64a3f32b361723e2a7efe
guxuerui <im.guxuerui@gmail.com>
2022-11-01 10:18:30 +0800

    update file:  base.lua

--- .config/nvim/lua/base.lua
+++ .config/nvim/lua/base.lua
@@ -8,6 +8,7 @@ vim.wo.number = true
 
 vim.opt.title = true
 vim.opt.autoindent = true
+vim.opt.smartindent = true
 vim.opt.hlsearch = true
 vim.opt.backup = false
 vim.opt.showcmd = true
@@ -18,15 +19,13 @@ vim.opt.scrolloff = 10
 vim.opt.shell = 'zsh'
 vim.opt.backupskip = '/tmp/*, /private/tmp/*'
 vim.opt.inccommand = 'split'
-vim.opt.ignorecase = true
+vim.opt.ignorecase = true -- Case insensitive searching UNLESS /C or capital in search
 vim.opt.smarttab = true
 vim.opt.breakindent = true
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
-vim.opt.ai = true -- Auto indent
-vim.opt.si = true -- Smart indent
 vim.opt.wrap = false -- No wrap lines
-vim.opt.backspace = 'start,eol,indent'
+vim.opt.backspace = 'start, eol, indent'
 vim.opt.path:append { '**' } -- Finding files - Search down into subfolders
 vim.opt.wildignore:append { '*/node_modules/*' }
 
@@ -35,3 +34,11 @@ vim.cmd([[let &t_Cs = "\e[4:3m"]])
 vim.cmd([[let &t_Ce = "\e[4:0m"]])
 -- but this dosen't work on iTerm2. Need to verify.
 
+-- Turn off paste mode when leaving insert
+vim.api.nvim_create_autocmd("InsertLeave", {
+  pattern = '*',
+  command = "set nopaste"
+})
+
+-- Add asterisks in block comments
+vim.opt.formatoptions:append { 'r' }
