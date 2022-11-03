0908552967d04cc31cd3b5db04e3dbd3de96ac02
guxuerui <im.guxuerui@gmail.com>
2022-11-03 22:56:15 +0800

    new file: options.lua

--- .config/nvim/lua/base.lua
+++ .config/nvim/lua/base.lua
@@ -1,52 +1,3 @@
-vim.cmd('autocmd!')
-vim.cmd('syntax on')
-
-vim.scriptencoding = 'utf-8'
-vim.opt.encoding = 'utf-8'
-vim.opt.fileencoding = 'utf-8'
-
-if (vim.fn.has('termguicolors') = 1) then
-  vim.opt.termguicolors = true
-end
-vim.wo.number = true
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.shiftround = true
-vim.opt.updatetime = 100
-vim.opt.cursorline = true
-vim.opt.autowrite = true
-vim.opt.title = true
-vim.opt.autoindent = true
-vim.opt.smartindent = true
-vim.opt.hlsearch = true
-vim.opt.backup = false
-vim.opt.showcmd = true
-vim.opt.cmdheight = 1
-vim.opt.laststatus = 2
-vim.opt.expandtab = true
-vim.opt.scrolloff = 10
-vim.opt.shell = 'zsh'
-vim.opt.backupskip = '/tmp/*, /private/tmp/*'
-vim.opt.inccommand = 'split'
-vim.opt.ignorecase = true -- Case insensitive searching UNLESS /C or capital in search
-vim.opt.smarttab = true
-vim.opt.breakindent = true
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.wrap = false -- No wrap lines
-vim.opt.backspace = 'start,eol,indent'
-vim.opt.path:append { '**' } -- Finding files - Search down into subfolders
-vim.opt.wildignore:append { '*/node_modules/*' }
-
--- Undercurl
-vim.cmd([[let &t_Cs = "\e[4:3m"]])
-vim.cmd([[let &t_Ce = "\e[4:0m"]])
--- but this dosen't work on iTerm2. Need to verify.
-
--- Add asterisks in block comments
-vim.opt.formatoptions:append { 'r' }
-
 -- disable some useles standard plugins to save startup time
 -- these features have been better covered by plugins
 vim.g.loaded_matchparen        = 1
@@ -63,3 +14,4 @@ vim.g.loaded_netrw             = 1
 vim.g.loaded_netrwPlugin       = 1
 vim.g.loaded_tutor_mode_plugin = 1
 vim.g.loaded_remote_plugins    = 1
+
