507f3479bb89c12ab67c0c2611d159738d61d3d3
guxuerui <im.guxuerui@gmail.com>
2023-04-28 12:36:44 +0800

    fix(config.fish): remove unused PATH entry and use nvm.fish for node version manager with default 18.14.2

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,8 @@
-set -g -x PATH ~/bin /opt/homebrew/bin /Users/xueruigu/.nvm/versions/node/v17.7.2/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin /Library/Apple/usr/bin $PATH
+# set -g -x PATH ~/bin /opt/homebrew/bin /Users/xueruigu/.nvm/versions/node/v17.7.2/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin /Library/Apple/usr/bin $PATH
+set -g -x PATH ~/bin /opt/homebrew/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin /Library/Apple/usr/bin $PATH
 set -g fish_user_paths "/Applications/Visual Studio Code.app/Contents/Resources/app/bin" $fish_user_paths
 set -gx EDITOR nvim
+set --universal nvm_default_version v18.14.2
 
 set -gx OPENAI_KEY "sk-......."
 starship init fish | source
