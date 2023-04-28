35562dfbd2d6ec1f604345ac77d57607b18c66f6
guxuerui <im.guxuerui@gmail.com>
2023-04-28 12:35:32 +0800

    ✨ feat(nvm.fish): add support for nvm version management in fish shell

---
+++ fish/functions/__check_nvm.fish
@@ -0,0 +1,14 @@
+function __check_nvm --on-variable PWD --description 'Do nvm stuff'
+  if test -f .nvmrc
+    set node_version (node -v)
+    set nvmrc_node_version (nvm list | grep (cat .nvmrc))
+
+    if set -q $nvmrc_node_version
+      nvm install
+    else if string match -q -- "*$node_version" $nvmrc_node_version
+      # already current node version
+    else
+      nvm use
+    end
+  end
+end
